119e13b1a602e2da8862d09f5566c21d4af40a1a
Joshua <data@shard.wtf>
2024-09-10 13:06:28 +0200

    update twconfig for nbrtlsm

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,9 +10,9 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        main: "#a388ee",
-        mainAccent: "#9e66ff", // not needed for shadcn components
-        overlay: "rgba(0,0,0,0.8)", // background color overlay for alert dialogs, modals, etc.
+        main: "#bd52eb",
+        mainAccent: "#a81be4", // not needed for shadcn components
+        overlay: "rgba(0,0,0,0.7)", // background color overlay for alert dialogs, modals, etc.
 
         // light mode
         bg: "#e3dff2",
@@ -39,74 +39,11 @@ const config: Config = {
         reverseBoxShadowY: "-6px",
       },
       fontWeight: {
-        base: "500",
-        heading: "700",
+        base: "400",
+        heading: "900",
       },
     },
   },
   plugins: [require("tailwindcss-animate")],
 };
 export default config;
-// import type { Config } from "tailwindcss";
-
-// const config: Config = {
-//   darkMode: ["class"],
-//   content: [
-//     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-//     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-//     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-//   ],
-//   theme: {
-//     extend: {
-//       colors: {
-//         background: "hsl(var(--background))",
-//         foreground: "hsl(var(--foreground))",
-//         card: {
-//           DEFAULT: "hsl(var(--card))",
-//           foreground: "hsl(var(--card-foreground))",
-//         },
-//         popover: {
-//           DEFAULT: "hsl(var(--popover))",
-//           foreground: "hsl(var(--popover-foreground))",
-//         },
-//         primary: {
-//           DEFAULT: "hsl(var(--primary))",
-//           foreground: "hsl(var(--primary-foreground))",
-//         },
-//         secondary: {
-//           DEFAULT: "hsl(var(--secondary))",
-//           foreground: "hsl(var(--secondary-foreground))",
-//         },
-//         muted: {
-//           DEFAULT: "hsl(var(--muted))",
-//           foreground: "hsl(var(--muted-foreground))",
-//         },
-//         accent: {
-//           DEFAULT: "hsl(var(--accent))",
-//           foreground: "hsl(var(--accent-foreground))",
-//         },
-//         destructive: {
-//           DEFAULT: "hsl(var(--destructive))",
-//           foreground: "hsl(var(--destructive-foreground))",
-//         },
-//         border: "hsl(var(--border))",
-//         input: "hsl(var(--input))",
-//         ring: "hsl(var(--ring))",
-//         chart: {
-//           "1": "hsl(var(--chart-1))",
-//           "2": "hsl(var(--chart-2))",
-//           "3": "hsl(var(--chart-3))",
-//           "4": "hsl(var(--chart-4))",
-//           "5": "hsl(var(--chart-5))",
-//         },
-//       },
-//       borderRadius: {
-//         lg: "var(--radius)",
-//         md: "calc(var(--radius) - 2px)",
-//         sm: "calc(var(--radius) - 4px)",
-//       },
-//     },
-//   },
-//   plugins: [require("tailwindcss-animate")],
-// };
-// export default config;
